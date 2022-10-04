ef81e46c0bd1a981479877a6a0db9fc0f2daee7f
ryuji <dragonogawaz@gmail.com>
2022-10-05 00:57:07 +0900

    ヘッダーのマークアップ変更

--- header.php
+++ header.php
@@ -10,11 +10,11 @@
 
 <body>
     <header class="header">
-        <div class="header-left">
+        <div class="header__left">
             <?php if (is_front_page()) : ?>
-                <h1 class="header__logo"><img src="<?php echo get_template_directory_uri() ?>/img/common/logo.png" alt="Engress"></h1>
+                <h1 class="header__logo"><img src="<?php echo get_template_directory_uri() ?>/img/common/logo-header.png" alt="Engress"></h1>
             <?php else : ?>
-                <div class="header__logo"><img src="<?php echo get_template_directory_uri() ?>/img/common/logo.png" alt="Engress"></div>
+                <div class="header__logo"><img src="<?php echo get_template_directory_uri() ?>/img/common/logo-header.png" alt="Engress"></div>
             <?php endif; ?>
             <?php
             wp_nav_menu(array(
@@ -29,13 +29,13 @@
             ));
             ?>
         </div>
-        <div class="header-right">
+        <div class="header__right">
             <div class="header__info">
-                <div class="header__info-hours">平日08:00〜20:00</div>
-                <div class="header__info-number">0123-456-7890</div>
+                <div class="header__hours">平日08:00〜20:00</div>
+                <div class="header__tel">0123-456-7890</div>
             </div>
             <div class="header__btn">
-                <a class="request-btn header-request-btn" href="">資料請求</a>
+                <a class="request-btn request-btn--header" href="">資料請求</a>
                 <a class="contact-btn" href="">お問い合わせ</a>
             </div>
             <button id="js-draw-btn" class="draw-btn" type="button">
@@ -46,19 +46,20 @@
         </div>
     </header>
     <nav id="js-draw-nav" class="draw-nav">
-        <?php
-        wp_nav_menu(array(
-            'theme_location' => 'draw-nav',
-            'container' => 'div',
-            'container_class' => 'draw-nav__body',
-            'menu_class' => 'draw-nav__list',
-            'add_li_class' => 'draw-nav__item',
-            'add_a_class' => 'draw-nav__link',
-            'items_wrap' => '<ul class="%2$s">%3$s</ul>',
-        ));
-        ?>
-        <div class="draw-nav__btn">
-            <a class="request-btn header-request-btn" href="">資料請求</a>
-            <a class="contact-btn" href="">お問い合わせ</a>
+        <div class="draw-nav__inner">
+            <?php
+            wp_nav_menu(array(
+                'theme_location' => 'draw-nav',
+                'container' => 'false',
+                'menu_class' => 'draw-nav__list',
+                'add_li_class' => 'draw-nav__item',
+                'add_a_class' => 'draw-nav__link',
+                'items_wrap' => '<ul class="%2$s">%3$s</ul>',
+            ));
+            ?>
+            <div class="draw-nav__btn">
+                <a class="request-btn request-btn--draw" href="">資料請求</a>
+                <a class="contact-btn contact-btn--draw" href="">お問い合わせ</a>
+            </div>
         </div>
     </nav>
