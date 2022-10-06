528557189594dcf8a23c424670ae097dbcfd0531
ryuji <dragonogawaz@gmail.com>
2022-10-06 23:57:51 +0900

    デフォルトの投稿のアーカイブページを有効にする記述を削除

--- contexts/global/customize.php
+++ contexts/global/customize.php
@@ -31,13 +31,3 @@ function add_nav_menu_class_link($classes, $item, $args)
     return $classes;
 }
 add_filter('nav_menu_link_attributes', 'add_nav_menu_class_link', 1, 3);
-
-function post_has_archive($args, $post_type)
-{
-    if ('post' == $post_type) {
-        $args['rewrite'] = true;
-        $args['has_archive'] = 'blog';
-    }
-    return $args;
-}
-add_filter('register_post_type_args', 'post_has_archive', 10, 2);
