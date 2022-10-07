d574f852f4eb2f820d3097e4003dcd6fd80469bf
ryuji <dragonogawaz@gmail.com>
2022-10-07 21:49:21 +0900

    下層ページ用のCSS読み込み

--- functions.php
+++ functions.php
@@ -2,3 +2,6 @@
 require_once get_theme_file_path('contexts/global/init.php', true);
 require_once get_theme_file_path('contexts/global/scripts.php', true);
 require_once get_theme_file_path('contexts/global/customize.php', true);
+
+// template
+require_once get_theme_file_path('contexts/template/scripts.php', true);
