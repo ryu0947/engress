fb837092e86eb8669a99e9a16d3d1d8b1b24eb9e
ryuji <dragonogawaz@gmail.com>
2022-10-08 11:19:10 +0900

    パンくずリストを読み込み

--- category.php
+++ category.php
@@ -1,3 +1,4 @@
 <?php get_header(); ?>
+<?php get_template_part("contexts/template/breadccrumb"); ?>
 <?php get_template_part("contexts/template/cta"); ?>
 <?php get_footer(); ?>
--- page-blog.php
+++ page-blog.php
@@ -1,4 +1,5 @@
 <?php get_header(); ?>
 <?php get_template_part("contexts/template/lower-mv"); ?>
+<?php get_template_part("contexts/template/breadccrumb"); ?>
 <?php get_template_part("contexts/template/cta"); ?>
 <?php get_footer(); ?>
