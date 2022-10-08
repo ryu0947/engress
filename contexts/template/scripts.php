93c1d4b8644df3afe3577d4c73b60a21e40f5b79
ryuji <dragonogawaz@gmail.com>
2022-10-08 11:21:39 +0900

    パンくずリスト用のCSS読み込み

--- contexts/template/scripts.php
+++ contexts/template/scripts.php
@@ -2,7 +2,11 @@
 function add_template_parts_scripts_files()
 {
     if (!(is_front_page() || is_single("blog") || is_single("news"))) {
-        wp_enqueue_style('template-style', get_template_directory_uri() . '/contexts/template/css/lower.css', array(), filemtime(get_theme_file_path('/contexts/template/css/lower.css')), 'all');
+        wp_enqueue_style('lower-mv-style', get_template_directory_uri() . '/contexts/template/css/lower-mv.css', array(), filemtime(get_theme_file_path('/contexts/template/css/lower-mv.css')), 'all');
+    }
+
+    if(!(is_front_page())) {
+        wp_enqueue_style('breadcrumb-style', get_template_directory_uri() . '/contexts/template/css/breadcrumb.css', array(), filemtime(get_theme_file_path('/contexts/template/css/breadcrumb.css')), 'all');
     }
 }
 
