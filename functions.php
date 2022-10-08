93fdff207649724847cc395c35b68fb9b2cb5608
ryuji <dragonogawaz@gmail.com>
2022-10-08 18:22:19 +0900

    ブログ記事一覧ページのCSS読み込み

--- functions.php
+++ functions.php
@@ -5,3 +5,6 @@ require_once get_theme_file_path('contexts/global/customize.php', true);
 
 // template
 require_once get_theme_file_path('contexts/template/scripts.php', true);
+
+// blog
+require_once get_theme_file_path('contexts/blog/scripts.php', true);
