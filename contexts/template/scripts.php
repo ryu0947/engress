b4cdbeff2c6a29d38b5630f1d1b79ad06577787c
ryuji <dragonogawaz@gmail.com>
2022-10-10 00:48:52 +0900

    ページネーション用のCSSの読み込み

--- contexts/template/scripts.php
+++ contexts/template/scripts.php
@@ -5,8 +5,12 @@ function add_template_parts_scripts_files()
         wp_enqueue_style('lower-mv-style', get_template_directory_uri() . '/contexts/template/css/lower-mv.css', array(), filemtime(get_theme_file_path('/contexts/template/css/lower-mv.css')), 'all');
     }
 
-    if(!(is_front_page())) {
+    if (!(is_front_page())) {
         wp_enqueue_style('breadcrumb-style', get_template_directory_uri() . '/contexts/template/css/breadcrumb.css', array(), filemtime(get_theme_file_path('/contexts/template/css/breadcrumb.css')), 'all');
+    }
+
+    if (is_page("blog") || is_singular("news")) {
+        wp_enqueue_style('pagenation-style', get_template_directory_uri() . '/contexts/template/css/pagenation.css', array(), filemtime(get_theme_file_path('/contexts/template/css/pagenation.css')), 'all');
     }
 }
 
