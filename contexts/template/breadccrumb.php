dce50d8a45f8164862af6dc30d3c28be4e1719ab
ryuji <dragonogawaz@gmail.com>
2022-10-10 10:55:38 +0900

    不要ファイル削除

--- contexts/template/breadccrumb.php
+++
@@ -1,9 +0,0 @@
-<div class="breadcrumb">
-    <div class="inner">
-        <?php
-        if (function_exists('bcn_display')) {
-            bcn_display();
-        }
-        ?>
-    </div>
-</div>
--- contexts/template/lower-mv.php
+++
@@ -1,3 +0,0 @@
-<div class="mv-lower mv-lower--blog">
-    <h1 class="mv-lower__title"><?php the_title(); ?></h1>
-</div>
