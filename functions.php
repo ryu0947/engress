110567de56c9856523c3463863ee9f6079f4ce01
ryuji <dragonogawaz@gmail.com>
2022-10-19 01:08:01 +0900

    トップページ用のCSS読み込み

--- functions.php
+++ functions.php
@@ -3,6 +3,9 @@ require_once get_theme_file_path('contexts/global/init.php', true);
 require_once get_theme_file_path('contexts/global/scripts.php', true);
 require_once get_theme_file_path('contexts/global/customize.php', true);
 
+// front
+require_once get_theme_file_path('contexts/front/scripts.php', true);
+
 // template
 require_once get_theme_file_path('contexts/template/scripts.php', true);
 
