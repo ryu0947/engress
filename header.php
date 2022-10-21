de8ef9e2117a74fe39735e5f2673ea3199e423eb
ryuji <dragonogawaz@gmail.com>
2022-10-21 23:55:10 +0900

    ヘッダーのロゴをクリックしたらトップページへ戻れるようにリンクを設置

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
 <body>
     <header class="header">
         <div class="header__left">
-            <div class="header__logo"><img src="<?php echo get_template_directory_uri() ?>/img/common/logo-header.png" alt="Engress"></div>
+            <div class="header__logo"><a class="header__logo-link" href="<?php echo esc_url(home_url('/')); ?>"><img src="<?php echo get_template_directory_uri() ?>/img/common/logo-header.png" alt="Engress"></a></div>
             <?php
             wp_nav_menu(array(
                 'theme_location' => 'global-nav',
