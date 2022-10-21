822906726affbc35b115d3013651fc6cbaefaf7a
ryuji <dragonogawaz@gmail.com>
2022-10-21 23:54:06 +0900

    ドロワーメニューの処理実装

---
+++ contexts/global/js/common.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const drawBtn = document.getElementById("js-draw-btn");
+const drawNav = document.getElementById("js-draw-nav");
+const body = document.body;
+
+drawBtn.addEventListener("click", () => {
+  drawBtn.classList.toggle("is-active");
+  drawNav.classList.toggle("is-show");
+  body.classList.toggle("is-fixed");
+});
