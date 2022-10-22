5b3bf71330af2f7a87b0b33309e1886b9a36201d
ryuji <dragonogawaz@gmail.com>
2022-10-22 09:29:15 +0900

    アコーディオンメニューの動き実装

--- contexts/global/js/common.js
+++ contexts/global/js/common.js
@@ -12,3 +12,36 @@ drawBtn.addEventListener("click", () => {
 });
 
 // アコーディオンメニュー
+const faqTitle = document.querySelectorAll(".js-faq__title");
+const faqInner = document.querySelectorAll(".js-faq__item-inner");
+const faqText = document.querySelectorAll(".js-faq__text");
+
+faqTitle.forEach((title) => {
+  title.addEventListener("click", () => {
+    if (title.dataset.isOpen === "false") {
+      const targetHeight =
+        title.nextElementSibling.firstElementChild.clientHeight;
+      title.nextElementSibling.style.height = `${targetHeight}px`;
+
+      setTimeout(function () {
+        title.nextElementSibling.style.height = "auto";
+        title.dataset.isOpen = "true";
+      }, 500);
+
+      title.classList.add("is-open");
+    }
+
+    if (title.dataset.isOpen === "true") {
+      const targetHeight =
+        title.nextElementSibling.firstElementChild.clientHeight;
+      title.nextElementSibling.style.height = `${targetHeight}px`;
+
+      setTimeout(function () {
+        title.nextElementSibling.style.height = 0;
+        title.dataset.isOpen = "false";
+      }, 10);
+
+      title.classList.remove("is-open");
+    }
+  });
+});
